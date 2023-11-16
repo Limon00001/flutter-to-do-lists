e328b2c28c4afb11d70eede704a0c62cd0235385
Monayem_Hossain_Limon <limonhossain82@gmail.com>
2023-11-16 04:33:42 +0000

    initial commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,43 +1,89 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Curved Container'),
-        ),
-        body: Center(
-          child: Container(
-            width: 200.0,
-            height: 200.0,
-            child: Center(
-              child: Text(
-                'Curved Container and Button',
-                style: TextStyle(color: Colors.white),
-              ),
+    return const MaterialApp(
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Welcome to Basic Flutter Design'),
+      ),
+      body: Center(
+        child: Container(
+          width: 300.0,
+          height: 200.0,
+          decoration: const BoxDecoration(
+            color: Colors.black54,
+            borderRadius: BorderRadius.only(
+              topLeft: Radius.circular(20.0),
+              topRight: Radius.circular(20.0),
             ),
-            decoration: BoxDecoration(
-              color: Colors.blue,
-              borderRadius: BorderRadius.only(
-                topLeft: Radius.circular(20.0),
-                topRight: Radius.circular(20.0),
-              ),
+          ),
+          child: const Center(
+            child: Text(
+              'container, button, pop-up container',
+              style: TextStyle(color: Colors.white),
             ),
           ),
         ),
-        floatingActionButton: FloatingActionButton(
-          onPressed: () {
-            // Add your action here
-          },
-          child: Icon(Icons.add),
-          backgroundColor: Colors.red,
-        ),
-        floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
       ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          _showPopup(context);
+        },
+        backgroundColor: Colors.blueAccent,
+        child: const Icon(Icons.add),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      bottomNavigationBar: Padding(
+        padding:
+            const EdgeInsets.only(bottom: 0.0), // Adjust the padding as needed
+        child: BottomAppBar(
+          shape: const CircularNotchedRectangle(),
+          child: Container(
+            height: 56.0,
+          ),
+        ),
+      ),
+    );
+  }
+
+  void _showPopup(BuildContext context) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Text('Add Something'),
+          content: const Text('Hello! Welcome.'),
+          actions: <Widget>[
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              child: const Text('Close'),
+            ),
+          ],
+        );
+      },
     );
   }
 }
